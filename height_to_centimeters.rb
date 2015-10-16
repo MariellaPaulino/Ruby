9fa20a76662baadf3cffe1479433cafbad6302d2
Mariella Paulino <mariella@codeforprogress.org>
2015-10-16 23:04:28 +0000

    I am currently creating a program with variables that have integers and strings and does basic calculations

--- height_to_centimeters.rb
+++ height_to_centimeters.rb
@@ -3,3 +3,5 @@ a = 72
 
 
 puts a * 2.54
+
+#to have it run you have to type> ruby height_to_centimeters.rb
---
+++ imperial_to_metric.rb
@@ -0,0 +1,11 @@
+my_name = "Mariella Paulino"
+
+height_inches = 70 #hahaha not really, but whatever.
+
+weight_pounds = 125 #total wink ;) 
+
+height_centimeters= height_inches * 2.54
+
+weight_kilograms = weight_pounds * 0.453592 
+
+puts my_name + " is " + height_centimeters.to_s + " cm and " + weight_kilograms.to_s + "kg."
