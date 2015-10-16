4f0936da051b660766cf92e36afe6df3228bc9e2
Mariella Paulino <mariella@codeforprogress.org>
2015-10-16 23:37:32 +0000

    this is a new program that asks for name, inches, and weight, and then does calculations to give you a new input

--- imperial_to_metric.rb
+++ imperial_to_metric.rb
@@ -1,11 +1,29 @@
+def convert_inches_to_centimeters(x)
+    height_centimeters = x* 2.54
+    return height_centimeters
+end 
+
+
+
+puts "what is your name?"
+
 my_name = gets.chomp
 
-height_inches = 70 #hahaha not really, but whatever.
+puts "what is your height in inches"
+#this is asking for input.  
 
-weight_pounds = 125 #total wink ;) 
+height_inches = gets.chomp.to_i
+#this is changing the answer from string to integer 
 
-height_centimeters= height_inches * 2.54
+puts "what is your weight in pounds"
 
-weight_kilograms = weight_pounds * 0.453592 
+weight_pounds = gets.chomp.to_i
+#this is changing the answer from string to integer 
+
+# height_centimeters= height_inches * 2.54 >> this is changed because of the function we have above 
+height_centimeters = convert_inches_to_centimeters(height_inches)
+
+weight_kilograms = weight_pounds * 0.453592
 
 puts my_name + " is " + height_centimeters.to_s + " cm and " + weight_kilograms.to_s + "kg."
+
