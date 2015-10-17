526a73b993eb93117642f57324b7a61137c95abf
Mariella Paulino <mariella@codeforprogress.org>
2015-10-17 13:05:12 +0000

    #create an if program that created multiple variables but has a specific one for 23

--- hiking.rb
+++ hiking.rb
@@ -32,16 +32,37 @@
 #create an if program that uses operators within a function to go hiking 
 #=====================================
 
-puts "What is tomorrow's temperature?" #asks me a question 
-temp =gets.chomp.to_i #gets the answer to the question and translate is as an integer 
+# puts "What is tomorrow's temperature?" #asks me a question 
+# temp =gets.chomp.to_i #gets the answer to the question and translate is as an integer 
 
-def going_hiking(temp) #define the function 
-    if temp >= 50 #create a requirement to move forward 
-        puts "#{temp} degrees is awesome for hiking" #put this in there if the above is true 
+# def going_hiking(temp) #define the function 
+#     if temp >= 50 #create a requirement to move forward 
+#         puts "#{temp} degrees is awesome for hiking" #put this in there if the above is true 
+#     end #end the function 
+#     if temp < 50 #otherwise do this requirement to move forward 
+#         puts "#{temp} degrees is netflix and chill weather!" #put this in there if the above is true
+#     end  #end the function 
+# end #end the function 
+
+# puts going_hiking(temp) #put the statement that is true 
+
+#=====================================
+#create an if program that created multiple variables but has a specific one for 23 
+#=====================================
+
+puts "What is tomorrow's temperature?" #asks me a question 
+x =gets.chomp.to_i #gets the answer to the question and translate is as an integer 
+
+def going_hiking(x) #define the function 
+    if x >= 50 #create a requirement to move forward 
+        puts "#{x} degrees is awesome for hiking" #put this in there if the above is true 
     end #end the function 
-    if temp < 50 #otherwise do this requirement to move forward 
-        puts "#{temp} degrees is netflix and chill weather!" #put this in there if the above is true
+    if x < 50 #otherwise do this requirement to move forward 
+        puts "#{x} degrees is netflix and chill weather!" #put this in there if the above is true
     end  #end the function 
+    if x == 23
+        puts "its exactly 23 degrees"
+    end
 end #end the function 
 
-puts going_hiking(temp) #put the statement that is true 
+puts going_hiking(x) 
